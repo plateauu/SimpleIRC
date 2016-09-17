e1900d00bfabe081608a5247885f8f373bbf3694
plateauu <plateauu@o2.pl>
2016-09-17 19:24:16 +0200

    initial develop branch

---
+++ src/com/plateauu/talk/ClientTalkTester2.java
@@ -0,0 +1,10 @@
+package com.plateauu.talk;
+
+import com.plateauu.talk.client.TalkClient;
+
+public class ClientTalkTester2 {
+	public static void main(String[] args){
+		TalkClient client = new TalkClient("127.0.0.1", 13333, "Tosia");
+		
+	}
+}
